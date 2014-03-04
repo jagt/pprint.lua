06050c581c8f5f076ea03272da4f797430930ab7
jagt <jagt@live.com>
2014-03-04 23:20:23 +0800

    more tests; try setup travis-ci

--- tests/basics.lua
+++ tests/basics.lua
@@ -6,7 +6,7 @@ local simple = {
     ['empty-table'] = {},
     ['simple-array'] = {1, 2, 3, 4, 5},
     ['simple-dict'] = {a=1, b=2, c=3},
-    ['function'] = test,
+    ['function'] = function()end,
     ['thread'] = {__type='thread'},
     ['userdata'] = {__type='userdata'},
 }
@@ -22,7 +22,6 @@ assert_str_equal(pprint.pformat({a=2}),
 }]])
 end)
 
--- TODO really should be doing this by eval
 test('simple with default settings', function()
 assert_str_equal(pprint.pformat(simple), [[
 {
@@ -38,4 +37,55 @@ assert_str_equal(pprint.pformat(simple), [[
   string = 'and my head is my only house unless it rains.'
 }
 ]])
+end)
+
+test('simple show_all', function()
+assert_str_equal(pprint.pformat(simple, {show_all = true}), [===[
+{
+  boolean = true,
+  ['empty-table'] = {},
+  function = [[function 1]],
+  number = 253,
+  ['simple-array'] = { 1, 2, 3, 4, 5 },
+  ['simple-dict'] = {
+    a = 1,
+    b = 2,
+    c = 3
+  },
+  string = 'and my head is my only house unless it rains.',
+  thread = [[thread 1]],
+  userdata = [[userdata 1]]
+}
+]===])
+end)
+
+-- '[['' and ']] can't be wraped in between
+test('wrap strings', function()
+assert_str_equal(
+    pprint.pformat(
+        'these are my twisted words.',
+        {level_width = 10, wrap_string = true}
+    ),
+[==[[[these ar
+e my twist
+ed words.
+]]]==])
+end)
+
+test('show custom type as table', function()
+local t = {1, 2, 3, 4, 5, __type = 'moon' }
+assert_str_equal(type(t), 'moon')
+assert_str_equal(
+pprint.pformat(t),
+[[
+{ 1, 2, 3, 4, 5,
+  __type = 'moon'
+}
+]]
+)
+end)
+
+-- _G should be complex enough
+test('eval pformat _G', function()
+local s = loadstring('return '..pprint.pformat(s, {show_all = true}))()
 end)
